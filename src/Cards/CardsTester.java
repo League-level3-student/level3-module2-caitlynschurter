d34628d7701d5e8eb477a01acfa527bc040468fd
caitl <caitl@KittyBook2>
2020-10-05 13:30:39 -0700

    Started Cards, CardsTester, Deck. Continued SearchingAndSortingAlgorithms, Algorithms   10/5/20

---
+++ src/Cards/CardsTester.java
@@ -0,0 +1,30 @@
+package Cards;
+
+import java.util.ArrayList;
+
+import Cards.Card.Suits;
+
+public class CardsTester {
+
+	public static void main(String[] args) {
+		Deck d = new Deck();
+		for (Card c : d) {
+			System.out.println(c);
+		}
+
+		System.out.println("=================");
+		d.shuffle();
+		for (Card c : d) {
+			System.out.println(c);
+		}
+
+		ArrayList<Card> hand = d.deal(5);
+		System.out.println("==================");
+		for (Card c : hand) {
+			System.out.println(c);
+		}
+		
+		System.out.println(d.size());
+
+	}
+}
--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -3,7 +3,12 @@ package _02_More_Algorithms;
 import java.util.List;
 
 public class Algorithms {
-	public static int findBrokenEgg(List<String> eggs) {
-		return 0;
+	public static int findBrokenEgg(List<String> list) {
+		for(int i = 0; i < list.size(); i++) {
+			if(list.get(i).equals("cracked")) {
+				return i;
+			}
+		}
+		return -1;
 	}
 }
